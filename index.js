87e46c834f0fb6549aa73a971ac6a045e183cbf0
Steven Meads <sbmeads@gmail.com>
2017-08-30 21:57:53 -0700

    (fix) .gitignore file is now working

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+authRoutes(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
