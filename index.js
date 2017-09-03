e6425c7b0c3425bbb2e562a7cdef59e86ebdae88
Steven Meads <sbmeads@gmail.com>
2017-09-03 15:12:39 -0700

    (add) cors module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./database/db');
+const cors = require('cors');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
@@ -10,7 +11,10 @@ db.getClient();
 
 const app = express();
 
+app.use(cors());
+
 app.use(cookieSession({
+    // Cookie set to last for 30 days
     maxAge: 30 * 24 * 60 * 60 * 1000,
     keys: [keys.cookieKey]
   })
