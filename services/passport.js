4ec618142f10fb5c6e728e64dda66010d4f5c37a
Steven Meads <sbmeads@gmail.com>
2017-09-03 15:14:48 -0700

    (update) insert new user with returning user_id logic

--- services/passport.js
+++ services/passport.js
@@ -3,15 +3,15 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const db = require('../database/db');
 
+// Generates token for identifying user, token stored with cookie
 passport.serializeUser((user, done) => {
-  console.log('SERIALIZE USER', user.user_id);
   done(null, user.user_id);
 });
 
+// Pulls token from cookie to determine what user
 passport.deserializeUser((id, done) => {
-  db.query('SELECT user_id FROM users WHERE user_id = ($1)', [id])
+  db.query('SELECT * FROM users WHERE user_id = ($1)', [id])
     .then(user => {
-      console.log('DESERIALIZED USER', user);
       done(null, user);
     }).catch(err => console.log('error deserializing user', err));
 });
@@ -33,17 +33,13 @@ passport.use(
         profile.emails[0].value
       ];
 
-      console.log('profile id', profile.id);
-
       db.query('SELECT * FROM users WHERE google_id = ($1)', [profile.id], (err, existingUser) => {
         if (err) console.log('Error querying users table', err);
         if (existingUser.rows[0]) {
-          console.log('existing user', existingUser.rows[0]);
           done(null, existingUser.rows[0]);
         } else {
-          db.query('INSERT INTO users("google_id", "first_name", "last_name", "email") VALUES ($1, $2, $3, $4)', values, (err, user) => {
+          db.query('INSERT INTO users("google_id", "first_name", "last_name", "email") VALUES ($1, $2, $3, $4) RETURNING user_id', values, (err, user) => {
             if (err) console.error('Error inserting record into users table', err);
-            console.log('NEW USER', user.rows[0]);
             done(null, user.rows[0]);
           })
         }
