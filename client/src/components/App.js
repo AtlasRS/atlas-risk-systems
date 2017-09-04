4f656cee9dd1ea564cbca2bfef73d902eec3bbe8
Steven Meads <sbmeads@gmail.com>
2017-09-04 07:19:35 -0700

    (add) AssetNew route and Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const Navbar = () => <h2>Navbar</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const AssetNew = () => <h2>AssetNew</h2>
 const Landing = () => <h2>Landing</h2>
@@ -10,8 +11,10 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Route exact={true} path='/' component={Landing} />
-          <Route path='/dashboard' component={Dashboard} />
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <Route exact path='/assets' component={Dashboard} />
+          <Route path='/assets/new' component={AssetNew} />
         </div>
       </BrowserRouter>
     </div>
