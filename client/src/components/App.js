08a9bf4e25d2a6d5868cf80bed32e5ffabc24b4f
Steven Meads <sbmeads@gmail.com>
2017-09-04 07:54:09 -0700

    (update) Navbar component to Header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Navbar from './Navbar';
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const AssetNew = () => <h2>AssetNew</h2>
 const Landing = () => <h2>Landing</h2>
@@ -11,7 +11,7 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
-          <Navbar />
+          <Header />
           <Route exact path='/' component={Landing} />
           <Route exact path='/assets' component={Dashboard} />
           <Route path='/assets/new' component={AssetNew} />
