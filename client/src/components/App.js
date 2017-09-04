4ed5d061640d4d52279a9bbd727baea029a1e793
Steven Meads <sbmeads@gmail.com>
2017-09-04 06:58:24 -0700

    (add) import BrowserRouter and Route modules and add Landing component route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Dashboard = () => <h2>Dashboard</h2>
+const AssetNew = () => <h2>AssetNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
   return (
     <div>
-      Atlas Risk Systems
+      <BrowserRouter>
+        <Route path='/' component={Landing} />
+      </BrowserRouter>
     </div>
   );
 };
