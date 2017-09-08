726ca75bfb8d71c6e8d3462c052c07aad18f1c63
Steven Meads <sbmeads@gmail.com>
2017-09-08 10:33:20 -0700

    (update) as child component of AuthFrom

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,19 +2,16 @@ import React from 'react';
 
 const Signup = () => {
   return (
-    <div id='signup-container'>
-      <div id='signup-form'>
-        <h2>Atlas Risk Systems</h2>
-        <a href='/auth/google'>Signup With Google</a>
-        <a href='/auth/google'>Signup With LinkedIn</a>
-        <p>Or Signup With Email</p>
-        <input placeholder='First Name'></input>
-        <input placeholder='Last Name'></input>
-        <input placeholder='Email'></input>
-        <input placeholder='Password'></input>
-        <input placeholder='Verify password'></input>
-        <button>Sign Up</button>
-      </div>
+    <div className='authform'>
+      <a href='/auth/google'>Signup with Google</a>
+      <a href='/auth/google'>Signup with LinkedIn</a>
+      <p>Or Signup with Email</p>
+      <input placeholder='First Name'></input>
+      <input placeholder='Last Name'></input>
+      <input placeholder='Email'></input>
+      <input placeholder='Password'></input>
+      <input placeholder='Verify password'></input>
+      <button>Signup</button>
     </div>
   );
 }
