c0c3b5bf830b690a2faed9a9a85e9e9a9859e9a7
Steven Meads <sbmeads@gmail.com>
2017-09-08 08:10:53 -0700

    (add) logic to handle routing for production

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.use(passport.session());
 
 authRoutes(app);
 
+// How assets will be served in production
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve production assets
+  app.use(express.static('client/build'));
+  // Express will serve the index.html file if it doesn't recongize the route
+  const path = require('path');
+  app.get('*', (res, req) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
