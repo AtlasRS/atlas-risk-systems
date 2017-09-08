e577431b3f048b17bf6c69bbb81f7e351d82bac8
Steven Meads <sbmeads@gmail.com>
2017-09-08 15:04:45 -0700

    (add local authentication via passport

--- services/passport.js
+++ services/passport.js
@@ -1,7 +1,12 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt-nodejs');
 const keys = require('../config/keys');
 const db = require('../database/db');
+
+// Salt factor for hashing password
+const SALT_WORK_FACTOR = 10;
 
 // Generates token for identifying user, token stored with cookie
 passport.serializeUser((user, done) => {
@@ -39,7 +44,7 @@ passport.use(
           done(null, existingUser.rows[0]);
         } else {
           db.query('INSERT INTO users("google_id", "first_name", "last_name", "email") VALUES ($1, $2, $3, $4) RETURNING user_id', values, (err, user) => {
-            if (err) console.error('Error inserting record into users table', err);
+            if (err) console.error('Error inserting record into database', err);
             done(null, user.rows[0]);
           })
         }
@@ -47,3 +52,51 @@ passport.use(
     }
   )
 );
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+  },
+  (email, password, done) => {
+
+    const SALT = bcrypt.genSalt(SALT_WORK_FACTOR, (err, result) => {
+      if (err) console.error('Error generating salt', err);
+      return result;
+    });
+
+    const hashedpwd = bcrypt.hash(password, SALT, (err, result) => {
+      if (err) console.error('Error hashing data', err);
+      return result;
+    });
+
+    // const existingHash = db.query('SELECT ($1) FROM users WHERE email = ($2)', [HASH, email], (err, existingHash) => {
+    //   if (err) console.error('Error querying users table', err);
+    //   return existingHash;
+    // });
+    //
+    // const compare = bcrypt.compare(HASH, existingHash, (err, result) => {
+    //   if (err) console.error('Error comparing data', err);
+    //   return result;
+    // });
+
+    if (compare === true) {}
+
+    const values = [
+      req.body.first_name,
+      req.body.last_name,
+      email,
+      hashedpwd
+    ];
+
+    db.query('SELECT * FROM users WHERE email = ($1) AND password = $(2)', [email, hshedpwd], (err, existingUser) => {
+      if (err) console.error('Error querying users table', err);
+      if (existingUser.rows[0]) {
+        done(null, existingUser.rows[0]);
+      } else {
+        db.query('INSERT INTO users("first_name", "last_name", "email", "password") VALUES ($1, $2, $3, $4) RETURNING user_id', values, (err, user) => {
+          if (err) console.error('Error inserting record into database', err);
+          done(null, user.rows[0]);
+        })
+      }
+    })
+  }
+));
