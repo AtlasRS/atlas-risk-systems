3c55a8e8f8b24d039ac9647ae2fe15a0dbac58d2
Enrique Florendo <enriqueflorendo@yahoo.com>
2017-09-09 16:24:15 -0700

    commiting

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-// Determines what set up credentials to use per ENV
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
-} else {
-  module.exports = require('./dev');
-}
