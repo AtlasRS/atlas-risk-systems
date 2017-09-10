aff463cec09887cf5ca51903694a2ebe2dfa4ea2
Enrique Florendo <enriqueflorendo@yahoo.com>
2017-09-10 10:27:43 -0700

    styles signup

--- server/client/src/components/Signup.js
+++ server/client/src/components/Signup.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import Ionicon from 'react-ionicons';
 
 const Signup = () => {
   return (
     <div className='authform'>
-      <a href='/auth/google'>Signup with Google</a>
-      <a href='/auth/google'>Signup with LinkedIn</a>
+      <h2>Sign Up</h2>
+      <div className='btn-group'> 
+        <a href='/auth/google' className='btn primary' style={{backgroundColor: '#E04827'}}>
+          <Ionicon icon="ion-social-google" color="white" className='ion'/> Login with Google
+        </a>
+        <a href='/auth/google' className='btn primary' style={{backgroundColor: '#267DE2'}}>
+          <Ionicon icon="ion-social-linkedin" color="white" className='ion'/> Login with LinkedIn
+        </a>
+      </div>
+      <div className='clearfix' />
+
       <p>Or Signup with Email</p>
       <input placeholder='First Name'></input>
       <input placeholder='Last Name'></input>
       <input placeholder='Email'></input>
       <input placeholder='Password'></input>
       <input placeholder='Verify password'></input>
-      <button>Signup</button>
+      <div className='pull-right'>
+        <button className='btn primary'>Sign Up</button>
+      </div>
+      <div className='clearfix' />
     </div>
   );
 }
