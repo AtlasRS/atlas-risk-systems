85ca7e87d5fbe3ec6e5af455ae8a95fc6daff67b
Steven Meads <sbmeads@gmail.com>
2017-09-11 23:48:10 -0700

    (add) initial structure of AssetsReducer

--- client/src/reducers/AssetsReducer.js
+++ client/src/reducers/AssetsReducer.js
@@ -1,15 +1,15 @@
-import _ from "lodash";
-import { FETCH_ASSETS, FETCH_ASSET, DELETE_ASSET } from "../actions";
-
-export default function(state = {}, action) {
-  switch (action.type) {
-    case DELETE_ASSET:
-      return _.omit(state, action.payload);
-    case FETCH_ASSET:
-      return { ...state, [action.payload.data.id]: action.payload.data };
-    case FETCH_ASSETS:
-      return _.mapKeys(action.payload.data, "id");
-    default:
-      return state;
-  }
-}
+// import _ from "lodash";
+// import { FETCH_ASSETS, FETCH_ASSET, DELETE_ASSET } from "../actions";
+//
+// export default function(state = {}, action) {
+//   switch (action.type) {
+//     case DELETE_ASSET:
+//       return _.omit(state, action.payload);
+//     case FETCH_ASSET:
+//       return { ...state, [action.payload.data.id]: action.payload.data };
+//     case FETCH_ASSETS:
+//       return _.mapKeys(action.payload.data, "id");
+//     default:
+//       return state;
+//   }
+// }
