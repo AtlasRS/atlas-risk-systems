19157328ed651af471d71d1eeb47f5ac0e788d95
Steven Meads <sbmeads@gmail.com>
2017-09-12 23:35:54 -0700

    (add) authentication flow with jwt for local strategy

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,14 @@
-import { GET_USER, AUTH_LOCAL } from '../actions/types';
+import { GET_USER, AUTH_USER, UNAUTH_USER, AUTH_ERROR} from '../actions/types';
 
 export default function(state = null, action) {
   switch (action.type) {
     case GET_USER:
       return action.payload || false;
-    case AUTH_LOCAL:
+    case AUTH_USER:
+      return action.payload || false;
+    case UNAUTH_USER:
+      return action.payload || false;
+    case AUTH_ERROR:
       return action.payload || false;
     default:
       return state;
---
+++ server/controllers/authentication.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const User = mongoose.model('users');
+const jwt = require('jwt-simple');
+const keys = require('../config/keys');
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user.id, iat: timestamp }, keys.jwtKey);
+}
+
+exports.login = (req, res, next) => {
+  console.log('inside login');
+  res.send({ token: tokenForUser(req.user) });
+}
+
+exports.signup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email || !password) return res.status(422).send({ error: 'You must provide email and password' });
+
+  User.findOne({ email: email }, (err, existingUser) => {
+    if (err) return next(err);
+    if (existingUser) return res.status(422).send({ error: 'Email arleady exists' });
+
+    User.create(req.body, (err, user) => {
+      if (err) return res.status(418).send({ error: err });
+      return res.status(200).json({ token: tokenForUser(user) });
+    });
+  });
+
+}
