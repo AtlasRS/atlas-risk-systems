d66cbb46c8a16b77a5e97895a1f46ec18954a8bf
Steven Meads <sbmeads@gmail.com>
2017-09-13 11:36:14 -0700

    (refactor) abstract passport strategies from request routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,45 +2,32 @@ const passport = require('passport');
 const Authentication = require('../controllers/authentication');
 const passportService = require('../routes/authRoutes');
 
+// Passport authentication strategies
+const requireAuth = passport.authenticate('jwt', { session: false });
+const googleAuth = passport.authenticate('google', { scope: ['profile', 'email' });
+const googleAuthCallback = passport.authenticate('google');
+const loginAuth = passport.authenticate('local', { session: false });
+
 module.exports = app => {
-  app.get('/',
-    passport.authenticate('jwt', {
-      session: false
-    }),
-    (req, res) => {
-      res.send({ hi: 'there' });
-    }
-  );
-  // Google authentication
-  app.get('/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
-    })
-  );
+  app.get('/', requireAuth, (req, res) => {
+    res.send({ hi: 'there' });
+  });
+
+  // #### Google authentication ####
+  app.get('/auth/google', googleAuth);
   // Authorized redirect route specified in Google credentials
-  app.get('/auth/google/callback',
-    passport.authenticate('google'),
-    (req, res) => {
-      res.redirect('/assets'); // redirects back to the user's dashboard
-    }
-  );
+  app.get('/auth/google/callback', googleAuthCallback, (req, res) => {
+    res.redirect('/assets'); // redirects back to the user's dashboard
+  });
 
-  app.post('/api/login',
-    passport.authenticate('local', {
-      session: false
-    }),
-    Authentication.login
-  );
-
+  // #### Local Authntication ####
+  app.post('/api/login', loginAuth, Authentication.login);
   app.post('/api/signup', Authentication.signup);
 
   app.get('/api/logout', (req, res) => {
-    // logout is automatically attached to req object via passport
-    req.logout();
+    req.logout(); // logout is automatically attached to req object via passport
     res.redirect('/');
   });
 
-  app.get('/api/current_user', (req, res) => {
-    res.send(req.user);
-  })
+  app.get('/api/current_user', (req, res) => res.send(req.user));
 }
