cdfbd48751f531ce9d26b43fa7259d9175927164
Steven Meads <sbmeads@gmail.com>
2017-09-16 13:31:59 -0700

    (add) googleAuth handler with action

--- client/src/components/SocialLogins.js
+++ client/src/components/SocialLogins.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 import Ionicon from 'react-ionicons';
 
-const SocialLogins = () => {
-  return (
-    <div>
-      <div className='btn-group'>
-        <a href='/auth/google' className='btn primary' style={{backgroundColor: '#E04827'}}>
-          <Ionicon icon="ion-social-google" color="white" className='ion'/> Login with Google
-        </a>
-        <a href='/auth/google' className='btn primary' style={{backgroundColor: '#267DE2'}}>
-          <Ionicon icon="ion-social-linkedin" color="white" className='ion'/> Login with LinkedIn
-        </a>
+
+class SocialLogins extends Component {
+
+  handleGoogleAuth() {
+    this.props.googleAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='btn-group'>
+          <div onClick={this.handleGoogleAuth.bind(this)} className='btn primary' style={{backgroundColor: '#E04827'}}>
+            <Ionicon icon="ion-social-google" color="white" className='ion'/> Login with Google
+          </div>
+          <a href='/auth/google' className='btn primary' style={{backgroundColor: '#267DE2'}}>
+            <Ionicon icon="ion-social-linkedin" color="white" className='ion'/> Login with LinkedIn
+          </a>
+        </div>
+        <div className='clearfix' />
       </div>
-      <div className='clearfix' />
-    </div>
-  );
+    );
+  }
 }
 
-export default SocialLogins;
+export default connect(null, actions)(SocialLogins);
