c2db59e185555e14810aed73233bca927826ab94
Steven Meads <sbmeads@gmail.com>
2017-09-16 10:18:47 -0700

    (update) anchor tag to react router Link tag

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <div id='landing-container'>
       <h1>Atlas Risk Systems</h1>
       <h3>A streamlined process for managing your assets</h3>
-      <a href='/auth/signup' className='btn primary'>Sign Up</a>
+      <Link to='/auth/signup' className='btn primary'>Sign Up</Link>
     </div>
   )
 }
