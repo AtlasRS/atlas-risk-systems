17bc9a7f18a358372ca58a7469213989a05a4a94
Steven Meads <sbmeads@gmail.com>
2017-09-18 14:23:17 -0700

    (update) from abstracted route

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -5,12 +5,16 @@ import SignupForm from './form_components/SignupForm';
 class Signup extends Component {
   render() {
     return (
-      <div className='authform'>
-        <h2>Sign Up</h2>
-        <SocialLogins />
-        <h4 className='m-t-1'>Or Signup with Email</h4>
-        <SignupForm />
-        <div className='clearfix' />
+      <div className='container m-t-2'>
+        <div className='card' style={{maxWidth: '500px'}}>
+          <div className='authform'>
+            <h2>Sign Up</h2>
+            <SocialLogins />
+            <h4 className='m-t-1'>Or Signup with Email</h4>
+            <SignupForm />
+            <div className='clearfix' />
+          </div>
+        </div>
       </div>
     );
   }
