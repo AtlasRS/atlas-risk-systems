e007d61741319e6275f0bb403b960359353b8db4
Steven Meads <sbmeads@gmail.com>
2017-09-18 14:25:09 -0700

    (update) authRoutes for linkined in strategy

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,8 +4,10 @@ const passportService = require('../routes/authRoutes');
 
 // Passport authentication strategies as helpers
 const requireAuth = passport.authenticate('jwt', { session: false });
-const googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] });
-const googleAuthCallback = passport.authenticate('google', { failureRedirect: '/', session: false });
+const googleAuth = passport.authenticate('google', { session: false, scope: ['profile', 'email'] });
+const googleAuthCallback = passport.authenticate('google', { session: false });
+const linkedInAuth = passport.authenticate('linkedin', { session: false, scope: ['r_basicprofile', 'r_emailaddress'] });
+const linkedInAuthCallback = passport.authenticate('linkedin', { session: false });
 const loginAuth = passport.authenticate('local', { session: false });
 
 module.exports = app => {
@@ -17,6 +19,11 @@ module.exports = app => {
   app.get('/auth/google', googleAuth);
   // Authorized redirect route specified in Google credentials
   app.get('/auth/google/callback', googleAuthCallback, Authentication.login);
+
+  // #### LinkedIn authentication ####
+  app.get('/auth/linkedin', linkedInAuth);
+  // Authorized redirect route specified in LinkedIn credentials
+  app.get('/auth/linkedin/callback', linkedInAuthCallback, Authentication.login);
 
   // #### Local Authntication ####
   app.post('/api/login', loginAuth, Authentication.login);
