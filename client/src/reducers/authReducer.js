02520b2a592615dbdc955fc5a07a68463019719b
Steven Meads <sbmeads@gmail.com>
2017-09-22 19:15:27 -0700

    (update) case for persist/REHYDRATE type

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -2,10 +2,12 @@ import { AUTH_USER, UNAUTH_USER, AUTH_ERROR } from '../actions/types';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case "persist/REHYDRATE":
+      return { ...state, authenticated: action.payload.auth.authenticated, user: action.payload.auth.user }
     case AUTH_USER:
       return { ...state, authenticated: true, user: action.payload }
     case UNAUTH_USER:
-      return { ...state, authenticated: false, user: null };
+      return { ...state, authenticated: false, user: {} };
     case AUTH_ERROR:
       return { ...state, error: action.payload };
     default:
