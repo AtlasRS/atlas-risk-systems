63a2167f6355036597c321f99c86db34c876c512
Steven Meads <sbmeads@gmail.com>
2017-09-25 18:38:34 -0700

    (remove) persist/REHYDRATE type

--- client/src/reducers/EntitiesReducer.js
+++ client/src/reducers/EntitiesReducer.js
@@ -2,8 +2,6 @@ import { GET_ENTITY, GET_ENTITIES, POST_ENTITY } from '../actions/types';
 
 export default function(state = {}, action) {
   switch (action.type) {
-    case "persist/REHYDRATE":
-      return { ...state, entities: action.payload.entities.entities }
     case GET_ENTITY:
       return { ...state, entity: action.payload }
     case GET_ENTITIES:
