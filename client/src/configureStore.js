587dd1c286ac67736c5939411c8e549ffda139a3
Steven Meads <sbmeads@gmail.com>
2017-09-25 18:37:42 -0700

    update) remove persistState method into App.js

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,14 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import { autoRehydrate, persistStore } from 'redux-persist';
+import { autoRehydrate } from 'redux-persist';
 import reduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
-// import { loadState, saveState } from './utils/localStorage';
-// import throttle from 'lodash/throttle';
 import reducers from './reducers';
 
-
 const store = compose(applyMiddleware(reduxThunk, logger), autoRehydrate())(createStore)(reducers);
-
-persistStore(store);
 
 export default store;
