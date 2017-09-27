735dd1998cb165803f55a050bfa2c983c3e3fc9a
Steven Meads <sbmeads@gmail.com>
2017-09-26 22:53:25 -0700

    (update) AssetReducer with GET_ASSETS type

--- client/src/reducers/AssetsReducer.js
+++ client/src/reducers/AssetsReducer.js
@@ -1,21 +1,19 @@
-export default function() {
-  return [
-    { entity: "Denver Basecamp", type: "Car Loan", make: "Ford", model: "Bronco", vin: 49281736102, expires: "Dec 21, 2017", insured: "true" },
-    { entity: "Ski Lodge", type: "Car Loan", make: "Ford", model: "Raptor", vin: 23451736102, expires: "Dec 31, 2017", insured: "true" },
-    { entity: "Eagle Tower", type: "Car Loan", make: "Ford", model: "Explorer", vin: 49282345236, expires: "Dec 1, 2017", insured: "true" },
-    { entity: "Denver Basecamp", type: "Car Loan", make: "Ford", model: "Excursion", vin: 9245235736102, expires: "Dec 13, 2017", insured: "true" }
-  ];
-}
-
-// import { POST_ASSET } from '../actions/types';
-//
-// export default function(state = {}, action) {
-//   switch (action.type) {
-//     case "persist/REHYDRATE":
-//       return { ...state, assets: action.payload.assets }
-//     case POST_ASSET:
-//       return { ...state, assets: action.payload }
-//     default:
-//       return state;
-//   }
+// export default function() {
+//   return [
+//     { entity: "Denver Basecamp", type: "Car Loan", make: "Ford", model: "Bronco", vin: 49281736102, expires: "Dec 21, 2017", insured: "true" },
+//     { entity: "Ski Lodge", type: "Car Loan", make: "Ford", model: "Raptor", vin: 23451736102, expires: "Dec 31, 2017", insured: "true" },
+//     { entity: "Eagle Tower", type: "Car Loan", make: "Ford", model: "Explorer", vin: 49282345236, expires: "Dec 1, 2017", insured: "true" },
+//     { entity: "Denver Basecamp", type: "Car Loan", make: "Ford", model: "Excursion", vin: 9245235736102, expires: "Dec 13, 2017", insured: "true" }
+//   ];
 // }
+
+import { GET_ASSETS } from '../actions/types';
+
+export default function(state = {}, action) {
+  switch (action.type) {
+    case GET_ASSETS:
+      return { ...state, assets: action.payload }
+    default:
+      return state;
+  }
+}
