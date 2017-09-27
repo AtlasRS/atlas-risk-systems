fa78d2fe0a2cf2cb823848eacbc3c6b24d5fae09
Steven Meads <sbmeads@gmail.com>
2017-09-26 22:55:37 -0700

    (add) assets property to response object

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('users');
+const Token = mongoose.model('tokens');
 const jwt = require('jwt-simple');
 const keys = require('../config/keys');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
 
 function tokenForUser(user) {
   const timestamp = new Date().getTime();
@@ -9,10 +12,12 @@ function tokenForUser(user) {
 }
 
 exports.login = (req, res, next) => {
-  res.send({ user: req.user, token: tokenForUser(req.user), entities: req.entities });
+  console.log("INSIDE LOGIN", req.assets);
+  res.send({ user: req.user, token: tokenForUser(req.user), entities: req.entities, assets: req.assets });
   // res.render('authenticated.html', { token: tokenForUser(req.user) });
   // res.redirect(`/assets?token=${tokenForUser(req.user)}`);
 }
+
 
 exports.signup = (req, res, next) => {
   const email = req.body.email;
@@ -21,11 +26,24 @@ exports.signup = (req, res, next) => {
 
   User.findOne({ email: email }, (err, existingUser) => {
     if (err) return next(err);
-    if (existingUser) return res.status(422).send({ error: 'Email arleady exists' });
+    if (existingUser) return res.status(400).send({ msg: 'The email address you have entered is already associated with another account.' });
 
     User.create(req.body, (err, user) => {
-      if (err) return res.status(418).send({ error: err });
-      return res.status(200).json({ token: tokenForUser(user) });
+      if (err) return res.status(500).send({ msg: err.message });
+      // Create a verification token for this user
+      const token = new Token({ _user: user._id, token: crypto.randomBytes(16).toString('hex') });
+      // Save the verification token
+      token.save(err => {
+        if (err) { return res.status(500).send({ msg: err.message }); }
+        // Send the email
+        const transporter = nodemailer.createTransport({ service: 'Sendgrid', auth: { user: keys.sendGridUsername, pass: keys.sendGridKey } });
+        const mailOptions = { from: 'no-reply@atlasrs.com', to: user.email, subject: 'Account Verification Token', text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/confirmation\/' + token.token + '.\n' };
+        transporter.sendMail(mailOptions, err => {
+            if (err) { return res.status(500).send({ msg: err.message }); }
+            res.status(200).send('A verification email has been sent to ' + user.email + '.');
+        });
+      });
+      // return res.status(200).json({ token: tokenForUser(user) });
     });
   });
 
