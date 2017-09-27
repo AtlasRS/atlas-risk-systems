fbd426c0cfd7d675f0f6d50b5b6c1523ba1b7cc1
Steven Meads <sbmeads@gmail.com>
2017-09-26 22:51:37 -0700

    (add) getAssets action

--- client/src/actions/assets.js
+++ client/src/actions/assets.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
-import { POST_ASSET } from './types';
+import { GET_ASSETS } from './types';
 
 const token = localStorage.getItem('token');
 
 export const postAsset = (values, history) => dispatch => {
   axios.post('/api/asset', values, { 'headers': { 'authorization': token } })
     .then(res => {
-      dispatch({ type: POST_ASSET, payload: res.data });
+      dispatch({ type: GET_ASSETS, payload: res.data });
       history.push('/assets');
     })
     .catch(err => {
