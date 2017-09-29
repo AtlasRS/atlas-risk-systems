5a875aa810be3c8e86359f0ad663400819f0ac21
Steven Meads <sbmeads@gmail.com>
2017-09-29 16:57:00 -0700

    add POST_ENTITY action

--- client/src/actions/assets.js
+++ client/src/actions/assets.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
-import { GET_ASSETS } from './types';
+import { POST_ASSET } from './types';
 
 const token = localStorage.getItem('token');
 
 export const postAsset = (values, history) => dispatch => {
   axios.post('/api/asset', values, { 'headers': { 'authorization': token } })
     .then(res => {
-      dispatch({ type: GET_ASSETS, payload: res.data });
+      dispatch({ type: POST_ASSET, payload: res.data });
       history.push('/assets');
     })
     .catch(err => {
-      console.error('Erro posting new Asset');
+      console.error('not happening', err);
     })
-}
+};
