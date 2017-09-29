af5f6e73fe3143ea19a0f54fa0f5af9377311911
Steven Meads <sbmeads@gmail.com>
2017-09-28 17:02:19 -0700

    add PURGE_ASSETS and PURGE_ENTITIES types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
+// Authentication Action Types
 export const AUTH_USER = 'auth-user';
 export const UNAUTH_USER = 'unauth-user';
 export const AUTH_ERROR = 'auth-error';
@@ -5,5 +6,7 @@ export const AUTH_ERROR = 'auth-error';
 export const GET_ENTITY = 'get-entity';
 export const GET_ENTITIES = 'get-entities';
 export const POST_ENTITY = 'post-entity';
+export const PURGE_ENTITIES = 'purge-entities';
 // Asset Action Types
 export const GET_ASSETS = 'get-assets';
+export const PURGE_ASSETS = 'purge-assets';
