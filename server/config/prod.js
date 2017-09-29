dd5e91c904b098b60b9c89fb32e21e6293b64b70
Steven Meads <sbmeads@gmail.com>
2017-09-28 22:00:55 -0700

    add keys and secrets for SendGrid and LinkedIn OAuth

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,7 +2,12 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  linkedInClientID: process.env.LINKEDIN_CLIENT_ID,
+  linkedInClientSecret: process.env.LINKEDIN_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  jwtKey: process.env.JWT_KEY
+  jwtKey: process.env.JWT_KEY,
+  sendGridKey: process.env.SENDGRID_KEY,
+  sendGridUsername: process.env.SENDGRID_USERNAME,
+  sendGridPassword: process.env.SENDGRID_PASSWORD
 };
