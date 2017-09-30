91817349e51d94146d566d2ecffd4b8cbccd6d38
Steven Meads <sbmeads@gmail.com>
2017-09-29 17:03:25 -0700

    remove getEntities middleware from post api/entity route

--- server/routes/entityRoutes.js
+++ server/routes/entityRoutes.js
@@ -8,9 +8,7 @@ const Entity = mongoose.model('entities');
 module.exports = app => {
   app.get('/api/entity/:id', requireAuth, EntityCtrl.getEntity);
   app.get('/api/entities/:id', requireAuth, EntityCtrl.getEntities);
-  app.post('/api/entity', requireAuth, EntityCtrl.postEntity, EntityCtrl.getEntities, (req, res) => {
-    return res.status(200).json(req.entities);
-  });
+  app.post('/api/entity', requireAuth, EntityCtrl.postEntity);
   app.put('/api/entity/:id', requireAuth, EntityCtrl.updateEntity);
   app.delete('/api/entity/:id', requireAuth, EntityCtrl.deleteEntity);
 };
