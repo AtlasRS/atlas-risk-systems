3362be581209d4c4b9a27ea04c7b100bc0c422c4
Steven Meads <sbmeads@gmail.com>
2017-09-29 22:18:02 -0700

    update redirect route to include entityID

--- client/src/actions/assets.js
+++ client/src/actions/assets.js
@@ -3,11 +3,11 @@ import { POST_ASSET } from './types';
 
 const token = localStorage.getItem('token');
 
-export const postAsset = (values, history) => dispatch => {
+export const postAsset = (values, entityID, history) => dispatch => {
   axios.post('/api/asset', values, { 'headers': { 'authorization': token } })
     .then(res => {
       dispatch({ type: POST_ASSET, payload: res.data });
-      history.push('/assets');
+      history.push(`/entity/assets/:${entityID}`);
     })
     .catch(err => {
       console.error('not happening', err);
