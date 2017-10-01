7696534e27938bd346cda20cd20cde7ff6455e57
Steven Meads <sbmeads@gmail.com>
2017-09-30 18:52:55 -0700

    add condition for proper history.push route

--- client/src/actions/assets.js
+++ client/src/actions/assets.js
@@ -3,11 +3,12 @@ import { POST_ASSET } from './types';
 
 const token = localStorage.getItem('token');
 
-export const postAsset = (values, entityID, history) => dispatch => {
+export const postAsset = (values, entityID, onEntity, history) => dispatch => {
   axios.post('/api/asset', values, { 'headers': { 'authorization': token } })
     .then(res => {
       dispatch({ type: POST_ASSET, payload: res.data });
-      history.push(`/entity/assets/:${entityID}`);
+      if (onEntity === false) history.push('/assets');
+      else history.push(`/entity/assets/:${entityID}`);
     })
     .catch(err => {
       console.error('not happening', err);
