d87c8b4c41f1420623ca58f5544b6fc1b5edce37
Steven Meads <sbmeads@gmail.com>
2017-10-02 16:42:33 -0600

    add ADD_NEW_ASSET action creator

--- client/src/actions/assets.js
+++ client/src/actions/assets.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { POST_ASSET } from './types';
+import { POST_ASSET, ADD_NEW_ASSET } from './types';
 
 const token = localStorage.getItem('token');
 
@@ -7,6 +7,7 @@ export const postAsset = (values, entityID, onEntity, history) => dispatch => {
   axios.post('/api/asset', values, { 'headers': { 'authorization': token } })
     .then(res => {
       dispatch({ type: POST_ASSET, payload: res.data });
+      dispatch({ type: ADD_NEW_ASSET, payload: res.data })
       if (onEntity === false) history.push('/assets');
       else history.push(`/entity/assets/:${entityID}`);
     })
