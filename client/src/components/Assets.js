24f0c812bcbc285593cf43e05ec5ccf0b1e1f0de
Steven Meads <sbmeads@gmail.com>
2017-10-04 23:06:36 -0600

    add ion-edit and ion-trash icons to assets

--- client/src/components/Assets.js
+++ client/src/components/Assets.js
@@ -37,7 +37,10 @@ class Assets extends Component {
             {asset.insured}
           </td>
           <td>
-            <Ionicon icon="ion-chevron-right" color="#222" fontSize="10px" className='ion'/>
+            <Ionicon icon="ion-edit" color="#222" fontSize="15px" className='ion'/>
+          </td>
+          <td>
+            <Ionicon icon="ion-trash-b" color="#222" fontSize="15px" className='ion'/>
           </td>
         </tr>
       );
