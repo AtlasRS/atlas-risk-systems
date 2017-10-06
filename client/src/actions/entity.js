441fffce71ea53482f91f309f83104af411e6ac3
Steven Meads <sbmeads@gmail.com>
2017-10-05 23:52:44 -0600

    update delete request

--- client/src/actions/entity.js
+++ client/src/actions/entity.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { GET_ENTITY, GET_ENTITIES, POST_ENTITY, DISPLAY_ENTITY_ASSETS, CLEAR_CURRENT_ENTITY } from './types';
+import { GET_ENTITY, GET_ENTITIES, POST_ENTITY, DISPLAY_ENTITY_ASSETS, CLEAR_CURRENT_ENTITY, DELETE_ENTITY } from './types';
 
 const token = localStorage.getItem('token');
 // Gets one entity
@@ -37,6 +37,17 @@ export const postEntity = (values, id, history) => dispatch => {
       console.error('not happening', err);
     })
 };
+
+export const deleteEntity = (entity_id, history) => dispatch => {
+  axios.delete(`/api/entity/${entity_id}`, { 'headers': { 'authorization': token } })
+    .then(res => {
+      dispatch({ type: DELETE_ENTITY, payload: entity_id });
+      history.push('/entities');
+    })
+    .catch(err => {
+      console.error('Did not delete entity', err);
+    })
+}
 
 export const displayEntityAssets = (entity_id, entity_name, assets, history) => dispatch => {
   dispatch({ type: DISPLAY_ENTITY_ASSETS, payload: { entity_id, entity_name, assets } });
