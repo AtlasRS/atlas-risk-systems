c712bc664f32ecc0230da371e1fb648525e61eba
Steven Meads <sbmeads@gmail.com>
2017-10-09 23:32:29 -0600

    add modal reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,13 +3,15 @@ import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 import AssetsReducer from './AssetsReducer';
 import EntitiesReducer from './EntitiesReducer';
+import ModalReducer from './ModalReducer';
 
 
 const rootReducer = combineReducers({
   auth: authReducer,
   form: formReducer,
   assets: AssetsReducer,
-  entities: EntitiesReducer
+  entities: EntitiesReducer,
+  modal: ModalReducer
 });
 
 export default rootReducer;
